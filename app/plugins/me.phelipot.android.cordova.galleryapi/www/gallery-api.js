32442b334b6b6c38c270b262c02d037a82ceec7f
pasterp <pascal.phelipot@gmail.com>
2019-04-07 18:06:16 +0200

    Added new plugin to access Gallery Data, Permissions and use native Toast

---
+++ app/plugins/me.phelipot.android.cordova.galleryapi/www/gallery-api.js
@@ -0,0 +1,68 @@
+
+function GalleryAPI()
+{
+    
+}
+
+GalleryAPI.prototype.checkPermission = function(successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'checkPermission',
+        []
+    );
+};
+
+
+GalleryAPI.prototype.getAlbums = function(successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getAlbums',
+        []
+    );
+};
+
+GalleryAPI.prototype.getMedias = function(successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getMedias',
+        []
+    );
+};
+
+GalleryAPI.prototype.getMedia = function(albumName, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getMedia',
+        [albumName]
+    );
+};
+
+GalleryAPI.prototype.getMediaThumbnail = function(media, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getMediaThumbnail',
+        [media]
+    );
+};
+
+GalleryAPI.prototype.getHQImageData = function(media, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getHQImageData',
+        [media]
+    );
+};
+
+module.exports = new GalleryAPI();
