a3cf8c1b0efe8972069e5816192ae630ab268dfe
pasterp <pascal.phelipot@gmail.com>
2019-04-07 23:24:54 +0200

    Fully implemented app

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,6 +1,8 @@
 var app = {
     preferences: {
-        api_url: 'http://192.168.0.12:8000'
+        api_url: 'http://192.168.0.12:8000',
+        threshold: 0.15,
+        history: []
     },
     tabs_list: ['history', 'gallery', 'results'],
     tabs : {
@@ -37,9 +39,12 @@ var app = {
         }
         this.setupParameters();
         this.setupGallery();
+        this.setupHistory();
     },
     initCallback: function () {
         document.getElementById('bTakePicture').onclick = this.takePicture;
+        document.getElementById('bFromGallery').onclick = this.galleryChooseImage;
+
 
         var front = document.getElementById('front-panel');
         var front_header = document.getElementById('front-panel-header').childNodes[0];
@@ -60,7 +65,6 @@ var app = {
             this.tabs[this.tabs_list[i]].onclick= this.changeActiveTabEvent.bind(app);
         }
     },
-
     setupCameraPreview: function () {
         var options = {
             x: 0,
@@ -105,8 +109,10 @@ var app = {
             if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
                 console.log("image sent ! Go to " + this.responseText);
                 app.toggleLoader();
-
+                
                 var url = JSON.parse(this.responseText)['Location'];
+                app.insertHistory(url);
+
                 app.fetchResult(url);
             } else if (this.readyState === XMLHttpRequest.DONE) {
                 console.error("Can't send the image! " + this.status + ':' + this.responseText);
@@ -136,10 +142,13 @@ var app = {
             var template = document.getElementById('results-template');
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                 var response = JSON.parse(this.responseText);
+                var date = response.date;
                 
-                template.innerHTML = '<div class="response"><div class="date">' + response.date + '</div>';
+                template.innerHTML = '<div class="response"><div class="date">Recherche du ' + date.slice(0, date.length-10) + '</div>';
                 for(result of response.results){
-                    template.innerHTML += '<div class="result"><img src="' + app.preferences.api_url + '/static' + result.image_url + '" /><p class="brand">' + result.marque + '</p><div class="score">' + result.score + '</div></div>'; 
+                    if(result.score > app.preferences.threshold){
+                        template.innerHTML += '<div class="result"><img src="' + app.preferences.api_url + '/static' + result.image_url + '" /><p class="brand">' + result.marque + '</p><div class="score">' + result.score + '</div></div>'; 
+                    }
                 }
                 
             } else if (this.readyState === XMLHttpRequest.DONE) {
@@ -151,13 +160,17 @@ var app = {
         xhr.send();
     },
     isPreferencesSet: function () {
-        return (localStorage.length > 0);
+        return (localStorage.length == 3);
     },
     loadPreferences: function () {
         this.preferences.api_url = localStorage.getItem('api_url');
+        this.preferences.history = JSON.parse(localStorage.getItem('history'));
+        this.preferences.threshold = localStorage.getItem('threshold');
     },
     savePreferences: function () {
         localStorage.setItem('api_url', this.preferences.api_url);
+        localStorage.setItem('history', JSON.stringify(this.preferences.history));
+        localStorage.setItem('threshold', this.preferences.threshold);
     },
     toggleLoader: function () {
         for (var element of document.getElementsByClassName('loader')) {
@@ -167,13 +180,13 @@ var app = {
     setupGallery: function(){
         galleryAPI.getMedias(function(images){
             var template = document.getElementById('gallery-template');
-            template.innerHTML = '<div class="gallery">'
+            template.innerHTML = '<div class="gallery">';
             for(var i = 0; i < images.length && i < 8; i++){
                 var image = images[i];
-                template.innerHTML += '<img src="data:image/png;base64,' + image.thumbnail +'" data-url="' + image.uri + '" />';
+                template.innerHTML += '<img src="data:image/png;base64,' + image.thumbnail +'" data-url="' + image.uri + '" onclick="app.galleryOpenImage(this)" />';
             }
-            //Add the browse button
-            template.innerHTML += '<div><i class="fas fa-images fa-10x"></div>';
+            //TODO: choose image from native gallery
+            template.innerHTML += '<div onclick="app.galleryChooseImage()"><i class="fas fa-images fa-10x"></div>';
             
             template.innerHTML += '</div>';
             app.reloadTabContent();
@@ -185,17 +198,59 @@ var app = {
         });
 
     },
+    galleryOpenImage: function(image){
+        data = image.src;
+        this.sendPicture(data);
+        
+    },
+    galleryChooseImage: function(){
+        navigator.camera.getPicture(app.sendPicture.bind(app), function(e){
+            console.error(e);
+            window.plugins.toast.showLongCenter("Impossible de récupérer cette image!");
+        },{
+            quality: 40,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+        });
+    },
+    setupHistory: function(){
+        var template = document.getElementById("history-template");
+        if(this.preferences.history.length == 0){
+            template.innerHTML = '<h3>Pas d\'historique.</h3>';
+            return;
+        }
+        template.innerHTML = "<p>Historique des recherches: </p><br><ul>";
+        for(req of this.preferences.history){
+            template.innerHTML += '<li data-url="' + req.url + '" onclick="app.loadHistory(this)"><i class="fas fa-eye fa-2x"></i>  ' + req.date + '</li>';
+        }
+        template.innerHTML += "</ul>";
+        app.reloadTabContent();
+    },
+    loadHistory: function(entry){
+        this.fetchResult(entry.dataset.url);
+    },
     setupParameters: function(){
         document.getElementById('params_url').value = this.preferences.api_url;
+        document.getElementById('params_threshold').value = this.preferences.threshold;
+
+    },
+    insertHistory: function(req_url){
+        this.preferences.history.push({
+            date: new Date().toLocaleString(),
+            url: req_url
+        });
+        this.savePreferences();
+        this.setupHistory();
     },
     saveParameters: function(){
         this.preferences.api_url = document.getElementById('params_url').value;
+        this.preferences.threshold = document.getElementById('params_threshold').value;
         this.savePreferences();
         window.plugins.toast.showShortCenter("Paramètres sauvegardés.");
         this.closeParameters()
     },
     openPanel: function() {
-        document.getElementById('front-panel').classList.toggle('up');
+        document.getElementById('front-panel').classList.toggle('up', true);
         document.getElementById('background').classList.toggle('hidden', true);
 
         var front_header = document.getElementById('front-panel-header').childNodes[0];
